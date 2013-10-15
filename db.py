3894962e83d01aa060e5db5cca9523c15ee9bbda
Benjamin Attal <attal.benjamin@gmail.com>
2013-10-15 19:57:34 -0400

    Initial commit

---
+++ db.py
@@ -0,0 +1,143 @@
+import sqlite3
+
+
+queries = {
+    'SELECT': 'SELECT %s FROM %s WHERE %s',
+    'INSERT': 'INSERT INTO %s VALUES(%s)',
+    'UPDATE': 'UPDATE %s SET %s WHERE %s',
+    'DELETE': 'DELETE FROM %s where %s',
+    'DELETE_ALL': 'DELETE FROM %s',
+    'CREATE_TABLE': 'CREATE TABLE IF NOT EXISTS %s(%s)',
+    'DROP_TABLE': 'DROP TABLE %s'}
+
+
+class DatabaseObject(object):
+
+    def __init__(self, data_file):
+        self.db = sqlite3.connect(data_file, check_same_thread=False)
+        self.data_file = data_file
+
+    def free(self, cursor):
+        cursor.close()
+
+    def write(self, query, values=None):
+        cursor = self.db.cursor()
+        if values is not None:
+            cursor.execute(query, list(values))
+        else:
+            cursor.execute(query)
+        self.db.commit()
+        return cursor
+
+    def read(self, query, values=None):
+        cursor = self.db.cursor()
+        if values is not None:
+            cursor.execute(query, list(values))
+        else:
+            cursor.execute(query)
+        return cursor
+
+    def select(self, tables, *args, **kwargs):
+        vals = ','.join(['?' for l in args])
+        locs = ','.join(tables)
+        conds = ' and '.join(['%s=?' % k for k in kwargs])
+        subs = [kwargs[k] for k in kwargs]
+        query = queries['SELECT'] % (vals, locs, conds)
+        return self.read(query, list(args) + subs)
+
+    def insert(self, table_name, *args):
+        values = ','.join(['?' for l in args])
+        query = queries['INSERT'] % (table_name, values)
+        return self.write(query, args)
+
+    def update(self, table_name, set_args, **kwargs):
+        updates = ','.join(['%s=?' % k for k in set_args])
+        conds = ' and '.join(['%s=?' % k for k in kwargs])
+        vals = [set_args[k] for k in set_args]
+        subs = [kwargs[k] for k in kwargs]
+        query = queries['UPDATE'] % (table_name, updates, conds)
+        return self.write(query, vals + subs)
+
+    def delete(self, table_name, **kwargs):
+        conds = ' and '.join(['%s=?' % k for k in kwargs])
+        subs = [kwargs[k] for k in kwargs]
+        query = queries['DELETE'] % (table_name, conds)
+        return self.write(query, subs)
+
+    def delete_all(self, table_name):
+        query = queries['DELETE_ALL'] % table_name
+        return self.write(query)
+
+    def create_table(self, table_name, values):
+        query = queries['CREATE_TABLE'] % (table_name, ','.join(values))
+        self.free(self.write(query))
+
+    def drop_table(self, table_name):
+        query = queries['DROP_TABLE'] % table_name
+        self.free(self.write(query))
+
+    def disconnect(self):
+        self.db.close()
+
+
+class Table(DatabaseObject):
+
+    def __init__(self, data_file, table_name, values):
+        super(Table, self).__init__(data_file)
+        self.create_table(table_name, values)
+        self.table_name = table_name
+
+    def select(self, *args, **kwargs):
+        return super(Table, self).select([self.table_name], *args, **kwargs)
+
+    def insert(self, *args):
+        return super(Table, self).insert(self.table_name, *args)
+
+    def update(self, set_args, **kwargs):
+        return super(Table, self).update(self.table_name, set_args, **kwargs)
+
+    def delete(self, **kwargs):
+        return super(Table, self).delete(self.table_name, **kwargs)
+
+    def delete_all(self):
+        return super(Table, self).delete_all(self.table_name)
+
+    def drop(self):
+        return super(Table, self).drop_table(self.table_name)
+
+
+class User(Table):
+
+    def __init__(self, data_file):
+        super(User, self).__init__(data_file, 'users',
+                                   ['username TEXT', 'password TEXT'])
+
+    def select(self, *args, **kwargs):
+        cursor = super(User, self).select(*args, **kwargs)
+        results = cursor.fetchall()
+        cursor.close()
+        return results
+
+    def insert(self, *args):
+        self.free(super(User, self).insert(*args))
+
+    def update(self, set_args, **kwargs):
+        self.free(super(User, self).update(set_args, **kwargs))
+
+    def delete(self, **kwargs):
+        self.free(super(User, self).delete(**kwargs))
+
+    def delete_all(self):
+        self.free(super(User, self).delete_all())
+
+    def drop(self):
+        self.free(super(User, self).drop())
+
+    def exists(self, username):
+        results = self.select('username', username=username)
+        return len(results) > 0
+
+    def authenticate(self, username, password):
+        results = self.select('username', username=username,
+                              password=password)
+        return len(results) > 0
